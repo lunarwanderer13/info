44c6e19a6ec3b93e471c51fae975f2f5ebb52da9
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-08-04 13:11:34 +0200

    Update vite config to fit the new repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwind from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pronouns/",
+  base: "/info/",
   plugins: [react(), tailwind()],
 })
