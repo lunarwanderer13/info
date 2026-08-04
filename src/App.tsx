b3ab21d5b38d66f32effa8bba9588f588c5defca
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-08-04 02:07:31 +0200

    Add main body

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 function App() {
   return (
-    <>
-      <h1 className="text-pink-500">Hello, World!</h1>
-    </>
+    <main className="h-screen bg-purple-950 flex items-center justify-center">
+      <div
+        className="
+          w-full h-full
+          md:w-3/4 md:h-3/4
+          md:max-w-200 md:max-h-200
+          md:rounded-2xl
+          bg-zinc-700
+          overflow-x-hidden overflow-y-auto
+        "
+      >
+
+      </div>
+    </main>
   )
 }
 
