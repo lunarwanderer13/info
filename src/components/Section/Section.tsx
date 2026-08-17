90c45180b44e10393f5d35997b0cd99a94fd72f5
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-08-17 20:46:41 +0200

    Update formatting style in the Section component

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -5,9 +5,7 @@ function Section({ header, description, children }: SectionProps) {
   return (
     <section className="w-full mt-20 px-10">
       <SectionHeader header={header} description={description} />
-      <section className="grid grid-cols-1 sm:grid-cols-2">
-        {children}
-      </section>
+      <section className="grid grid-cols-1 sm:grid-cols-2">{children}</section>
     </section>
   )
 }
